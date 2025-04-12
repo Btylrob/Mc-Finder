c16ee989ed4db1f81328026b1e0882633d48dc2c
Brandon Robinson <brantylrobinson@gmail.com>
2025-04-12 09:13:48 -0400

    add cuda gpuconfig

---
+++ app.py
@@ -0,0 +1,10 @@
+import tensorflow as tensorflow
+import os
+import cv2 
+from PIL import Image
+import numpy as np 
+from matplotlib import pyplot as plt 
+
+# GPU / CPU Mem config - I have a CPU however if you are running on GPU on comment out next line
+#CUDA_VISIBLE_DEVICES=0
+CUDA_VISIBLE_DEVICES=""
